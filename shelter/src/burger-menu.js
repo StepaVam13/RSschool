d623e1696a22ad4ab3bb03017411f9290e695e1e
StepaVam13 <stepavam13@gmail.com>
2024-09-15 16:53:59 +0300

    Burger-menu script

---
+++ shelter/src/burger-menu.js
@@ -0,0 +1,14 @@
+function openBurger() {
+
+	if(document.documentElement.clientWidth < 768) {
+		document.querySelector("body").classList.toggle("fix");
+		document.querySelector(".burger-menu").classList.toggle("active");
+		document.querySelector("nav").classList.toggle("open");
+  }
+
+}
+
+
+
+document.querySelector(".burger-menu").addEventListener("click", openBurger);
+document.querySelector("nav").addEventListener("click", openBurger);
